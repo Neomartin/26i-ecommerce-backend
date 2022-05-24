d5a25ff4ea005666dee5039c864750272d58aece
Martín Rodríguez <neomartinr@gmail.com>
2022-05-24 19:37:22 -0300

    Password encrypted

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,6 +1,8 @@
 
 const res = require('express/lib/response');
 const User = require('../schemas/user.schema');
+const bcrypt = require('bcrypt')
+const saltRounds = 10;
 
 function getUsers(req, res) {
     //users
@@ -28,24 +30,27 @@ function getUsers(req, res) {
 }
 
 async function getUser(req, res) {
+
     console.log(req.params)
+
     // Recibo como param una propiedad userID y la almacenos en la variable id
     const id = req.params.userID;
+
     // Hago una búsqueda con el método find en la DB pero mando un objecto como primera propiedad del find en el que especifico la propiedad _id tiene que ser igual al id que recibo params.
         //    // User.find({ _id: id }, (error, user) => {
         //    //     console.log(user)
         //    // })
+
     const user = await User.findById(id)
     if(!user) return res.status(200).send({
         ok: false,
         message: `No se encontró ningún usuario con ese id`
     })
-    console.log(`user`, user)
 
     return res.send({ 
         ok: true,
         message: 'Usuario encontrado',
-        user
+        user: user
     })
 }
 
@@ -55,8 +60,24 @@ async function createUser(req, res) {
         console.log(req.body);
         // Formateo la data proveniente en un documento compatible con mi base de datos MONGO
         let user = new User(req.body);
+        console.log(`Antes`, user)
+        let password = req.body.password;
 
-        console.log(user)
+        const encryptedPassword = await bcrypt.hash(password, saltRounds)
+        if(!encryptedPassword) {
+            return res.status(500).send({
+                ok: false,
+                message: 'Error al intentar guardar usuario'
+            })
+        }
+        console.log(`encryptedPassword`, encryptedPassword)
+        user.password = encryptedPassword
+
+
+        console.log(`Despues`, user)
+
+
+
         const newUser = await user.save();
 
         newUser.password = undefined;
@@ -81,42 +102,28 @@ async function createUser(req, res) {
     
 }
 
+async function deleteUser(req, res) {
+    console.log(req.params.userToDeleteId) //627d9128df3426364be30d5e
 
+    const id = req.params.userToDeleteId
 
+    const deletedUser = await User.findByIdAndDelete(id)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function deleteUser(req, res) {
     return res.status(200).send({
-        message: 'El usuario será BORRADO'
+        message: 'El usuario fue BORRADO',
+        deletedUser
     })
 }
 
-const updateUser = (req, res) => {
+const updateUser = async(req, res) => {
+    
+    const id = req.query.idToUpdate
+
+    const newUser = await User.findByIdAndUpdate(id, req.body, { new: true })
+
     return res.status(200).send({
-        message: 'El usuario será ACTUALIZADO'
+        message: 'El usuario fue ACTUALIZADO',
+        newUser
     })
 }
 
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -12,8 +12,12 @@ api.get('/users/:userID', userController.getUser);
 //Para escribir data en el backend se suele enviar con método POST a través del body
 api.post('/users', userController.createUser);
 
-api.delete('/users', userController.deleteUser)
+
+
+api.delete('/users/:userToDeleteId', userController.deleteUser);
+
 api.put('/users', userController.updateUser)
+
 api.post('/login', userController.login)
 
 module.exports = api
